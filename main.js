4175d0cdad814860b4869ba6113eb026f5298087
naroqwe <49810638+naroqwe@users.noreply.github.com>
2023-10-23 20:56:08 +0200

    almost finished

--- main.js
+++ main.js
@@ -2,28 +2,72 @@ const buttons = document.querySelectorAll(".number");
 const dot = document.querySelector(".dot");
 const equalSign = document.querySelector(".equals");
 const signOperator = document.querySelectorAll(".sign");
-const result = document.querySelector(".result");
+
+const firstNumber = document.querySelector(".firstNumber");
+const secondNumber = document.querySelector(".secondNumber");
+const mathSign = document.querySelector(".mathSign");
 const clearButton = document.querySelector(".clear");
 const deleteButton = document.querySelector(".delete");
 
-let firstVal;
-let secondVal;
-let mathSign;
+let result = "";
+
+function addToDisplay() {
+  firstNumber.innerHTML += this.textContent;
+}
+
+function asd() {
+  if (firstNumber.innerHTML === "") {
+    return;
+  }
+
+  // if (mathSign.innerHTML !== "") {
+  //   operate();
+  // }
+
+  secondNumber.innerHTML = firstNumber.innerHTML;
+  mathSign.innerHTML += this.textContent;
+  firstNumber.innerHTML = "";
+}
+
+function operate() {
+  let a = Number(firstNumber.innerHTML);
+  let b = Number(secondNumber.innerHTML);
+  let operatorSign = mathSign.innerHTML;
+
+  switch (operatorSign) {
+    case "+":
+      result = a + b;
+      break;
+    case "-":
+      result = a - b;
+      break;
+    case "/":
+      result = a / b;
+      break;
+    case "x":
+      result = a * b;
+      break;
+  }
+  firstNumber.innerHTML = result;
+  secondNumber.innerHTML = "";
+  mathSign.innerHTML = "";
+}
+
+function clearScreen() {
+  firstNumber.innerHTML = "";
+  secondNumber.innerHTML = "";
+  mathSign.innerHTML = "";
+  result.innerHTML = "";
+}
 
 buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    addToDisplay(button.textContent);
-    firstVal = Number(result.innerHTML);
-    console.log(firstVal);
-  });
+  button.addEventListener("click", addToDisplay);
 });
 
 signOperator.forEach((sign) => {
-  sign.addEventListener("click", () => {});
+  sign.addEventListener("click", asd);
 });
 
-function addToDisplay(number) {
-  result.innerHTML += number;
-}
+equalSign.addEventListener("click", operate);
 
-//push to values array, then extract them and calculate
+clearButton.addEventListener("click", clearScreen);
